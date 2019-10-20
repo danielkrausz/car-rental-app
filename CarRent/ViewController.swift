ff91b7e943f57d8781c7bacc29162c453ad07ee7
danielkrausz <dnielkrausz@gmail.com>
2019-10-21 01:17:31 +0200

    Initial Commit

---
+++ CarRent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarRent
+//
+//  Created by Dániel Krausz on 2019. 10. 21..
+//  Copyright © 2019. Dániel Krausz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
